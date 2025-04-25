b505f6a45c11029c97ff8eff84e5a652b731bbc4
rawabot63 <rawabot63@gmail.com>
2025-04-25 04:37:20 +0330

    auto push

--- push.bat
+++ push.bat
@@ -1,3 +1,18 @@
 git add .
 git commit -m "auto push"
 git push
+@echo off
+echo ---------------------------
+echo Pushing to GitHub & Deploy
+echo ---------------------------
+
+cd /d %~dp0
+
+git add .
+git commit -m "auto deploy"
+git push origin main
+
+echo ---------------------------
+echo Done! Check your Render.com
+echo ---------------------------
+pause
